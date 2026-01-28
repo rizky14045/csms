18b31dce52da966d0142e2b2a13c549b7c614bdb
alifnrz27 <alifnaufalrizki27@gmail.com>
2026-01-28 11:11:19 +0700

    [ADD] column created by, updated by, deleted by

---
+++ database/migrations/2026_01_28_110837_add_created_by_to_sub_areas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCreatedByToSubAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sub_areas', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by')->nullable()->after('created_at');
+            $table->unsignedBigInteger('updated_by')->nullable()->after('updated_at');
+            $table->unsignedBigInteger('deleted_by')->nullable()->after('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sub_areas', function (Blueprint $table) {
+            //
+        });
+    }
+}
